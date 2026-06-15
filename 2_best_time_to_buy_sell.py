db164f6c3b169c15c4d5e0fbb43f767293e233f7
Tanishka Waghmare <wtanishka55@gmail.com>
2026-06-15 23:10:58 +0530

    Create 2_best_time_to_buy_sell.py

---
+++ 2_best_time_to_buy_sell.py
@@ -0,0 +1,9 @@
+def max_profit(prices):
+    min_price = float('inf')
+    profit = 0
+
+    for price in prices:
+        min_price = min(min_price, price)
+        profit = max(profit, price - min_price)
+
+    return profit
