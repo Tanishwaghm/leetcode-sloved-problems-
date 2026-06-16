7f356a472486c7c4cb206c89f5a8c556d52c3281
Tanishka Waghmare <wtanishka55@gmail.com>
2026-06-16 08:23:21 +0530

    Create 74 .py

---
+++ 74 .py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+
+        mp = defaultdict(list)
+
+        for s in strs:
+            key = ''.join(sorted(s))
+            mp[key].append(s)
+
+        return list(mp.values())
